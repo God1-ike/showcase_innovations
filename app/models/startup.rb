0c3fab9a727739893c58d3827e76fec13c2e182b
Roman Salikov <roman.salikov@rt.ru>
2021-12-02 19:30:30 +0400

    fix state for startup

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,39 +1,40 @@
 # frozen_string_literal: true
 
 class Startup < ApplicationRecord
+  include AASM
   extend Enumerize
 
   has_one_attached :presentation
 
   enumerize :direction, in: %i[]
 
-  # aasm do
-  #   state :new, initial: true
-  #   state :screening, :scoring, :expert_council, :acceleration_program,
-  #         :piloting_program, :investing
+  aasm :state, column: :state do
+    state :new, initial: true
+    state :screening, :scoring, :expert_council, :acceleration_program,
+          :piloting_program, :investing
 
-  #   event :set_screening do
-  #     transitions to: :screening
-  #   end
+    event :set_screening do
+      transitions to: :screening
+    end
 
-  #   event :set_scoring do
-  #     transitions to: :scoring
-  #   end
+    event :set_scoring do
+      transitions to: :scoring
+    end
 
-  #   event :set_expert_council do
-  #     transitions to: :expert_council
-  #   end
+    event :set_expert_council do
+      transitions to: :expert_council
+    end
 
-  #   event :set_acceleration_program do
-  #     transitions to: :acceleration_program
-  #   end
+    event :set_acceleration_program do
+      transitions to: :acceleration_program
+    end
 
-  #   event :set_piloting_program do
-  #     transitions to: :piloting_program
-  #   end
+    event :set_piloting_program do
+      transitions to: :piloting_program
+    end
 
-  #   event :set_investing do
-  #     transitions to: :investing
-  #   end
-  # end
+    event :set_investing do
+      transitions to: :investing
+    end
+  end
 end
