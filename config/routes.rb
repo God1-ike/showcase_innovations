57725ace960ff9ab9ff6c69088eef087ae3dbf5a
aleksey.ryabchikov <al.ryabchikov@gmail.com>
2021-12-02 23:37:15 +0400

    Add change_state into sturtups_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   namespace :api do
-    resources :startups, only: %i[create update destroy index show]
+    resources :startups, only: %i[create update destroy index show change_state]
+    resources :pilots, only: %i[create update destroy index show]
   end
 
   match '/:path', to: 'welcome#index', via: :all, constraints: { path: /((?!rails).)*/ }
