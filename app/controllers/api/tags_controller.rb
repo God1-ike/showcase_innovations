1ced56158fc7e5764cb40d3af15a4a3411dfe045
Roman Salikov <roman.salikov@rt.ru>
2021-12-03 02:45:54 +0400

    fix limit for tags

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -1,6 +1,6 @@
 class Api::TagsController < ApplicationController
   def index
-    tags = Tag.limit(params: params[:limit])
+    tags = Tag.limit(params[:limit])
 
     render json: tags, each_serializer: TagSerializer
   end
