cfe2130bee58e3afbdcee75953a29a417ef07ff2
Александр Герасимов <a.a.gerasimov@rt.ru>
2021-12-02 19:17:38 +0400

    Fix bug

--- app/serializers/startup_serializer.rb
+++ app/serializers/startup_serializer.rb
@@ -1,6 +1,6 @@
 class StartupSerializer < ActiveModel::Serializer
   attributes :title, :description, :state, :contact_name, :phone_number, :email, :direction, :legal_entity_title,
-             :contact_rank, :inn, :people_count, :url, :presentation
+             :contact_rank, :inn, :people_count, :site_url, :presentation
 
   def presentation
     rails_storage_proxy_path(object.presentation)
