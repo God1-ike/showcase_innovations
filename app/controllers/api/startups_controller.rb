78b9ec707761880a1566cb77ed67c1c90bd4d189
Roman Salikov <roman.salikov@rt.ru>
2021-12-03 02:01:25 +0400

    startap filters

--- app/controllers/api/startups_controller.rb
+++ app/controllers/api/startups_controller.rb
@@ -1,13 +1,15 @@
 class Api::StartupsController < ApplicationController
   def index
     startups = Startup.all
-    tags, state, people_count, organization_transport = params.to_h.values_at(
-      :tags, :state, :people_count, :organization_transport
+    tags, state, people_count, organization_transport, business_segment, readiness = params.to_h.values_at(
+      :tags, :state, :people_count, :organization_transport, :business_segment, :readiness
     )
     startups = startups.by_tags(tags) if tags.present?
     startups = startups.where(state: state) if state.present?
     startups = startups.where("people_count >= ?", people_count) if people_count.present?
     startups = startups.where(organization_transport: organization_transport) if organization_transport.present?
+    startups = startups.where(business_segment: business_segment) if business_segment.present?
+    startups = startups.where(readiness: readiness) if readiness.present?
 
     render json: startups, each_serializer: StartupSerializer
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_210601) do
+ActiveRecord::Schema.define(version: 2021_12_02_215223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2021_12_02_210601) do
     t.string "presentation_url"
     t.string "organization_transport"
     t.string "business_segment"
+    t.string "readiness"
   end
 
   create_table "tags", force: :cascade do |t|
