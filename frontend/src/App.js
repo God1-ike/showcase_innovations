8e2733533716d622c077c65d111a669fba13ec97
Alexander Gerasimov <andarelph@gmail.com>
2021-12-02 20:58:13 +0400

    make frontend app with routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback } from 'react';
+import { Link, Route } from "wouter";
+import { Layout, Menu } from 'antd';
+import {
+  FormOutlined,
+  PieChartOutlined,
+} from '@ant-design/icons';
+import { StartupForm } from './pages/startup-form';
+import { Analytics } from './pages/analytics';
+
+const { Content, Sider } = Layout;
+
+const routes = [
+  {
+    title: 'Форма стартапа',
+    url: '/startup-form',
+    component: StartupForm,
+    icon: FormOutlined
+  },
+  {
+    title: 'Аналитика',
+    url: '/analytics',
+    component: Analytics,
+    icon: PieChartOutlined
+  },
+];
+
+const Router = () => {
+  return routes.map(route=><Route path={route.url} component={route.component} />);
+};
+
 
 function App() {
+  const [collapsed, setCollapsed] = useState(localStorage.getItem('isSiderCollapsed'));
+  const toggleCollapse = useCallback(() => {
+    setCollapsed(!collapsed);
+    localStorage.setItem('isSiderCollapsed', !collapsed);
+  }, [collapsed]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider theme='light' collapsible collapsed={collapsed} onCollapse={toggleCollapse}>
+          <Menu defaultSelectedKeys={[routes[0].url]} mode="inline">
+          {routes.map((route)=><Menu.Item key={route.url} icon={<route.icon />}>
+    <Link href={route.url}>{route.title}</Link>
+  </Menu.Item>)}
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Content style={{ margin: '0 16px' }}>
+            <Router />
+          </Content>
+        </Layout>
+      </Layout>
   );
 }
 
