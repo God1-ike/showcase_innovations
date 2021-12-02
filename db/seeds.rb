7addf54bdad5f728a96bd1900aaeb6a89a700683
Roman Salikov <roman.salikov@rt.ru>
2021-12-03 00:34:34 +0400

    add pilot seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,23 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
+require 'json'
 
-startups = YAML.load_file(Rails.root.join("config/startups.yml"))
+startups = JSON.parse(File.read(Rails.root.join("test/fixtures/startups.json")))
 
 startups.each do |startup_info|
   startup = Startup.find_or_initialize_by(email: startup_info[:email])
   startup.assign_attributes(startup_info)
-  startup.save
+  startup.save!
+end
+
+pilots = JSON.parse(File.read(Rails.root.join("test/fixtures/pilots.json")))
+
+pilots.each do |pilot_info|
+  startup = Startup.find_by(email: pilot_info.delete("startup_email"))
+  pilot_info.merge!(startup: startup)
+
+  pilot = Pilot.find_or_initialize_by(name: pilot_info[:name])
+  pilot.assign_attributes(pilot_info)
+  pilot.save!
 end
