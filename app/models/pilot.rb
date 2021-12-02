121b4e06517347f0cb277171d2ada2f9628c0b7e
aleksey.ryabchikov <al.ryabchikov@gmail.com>
2021-12-02 19:48:19 +0400

    Add state machine for pilot

--- app/models/pilot.rb
+++ app/models/pilot.rb
@@ -1,4 +1,66 @@
 class Pilot < ApplicationRecord
+  include AASM
+
   belongs_to :startup
 
+  aasm column: :state do
+    state :in_work, initial: true
+    state :suspended, :finished, :closed, :cancelled
+
+    event :set_suspended do
+      transitions to: :suspended
+    end
+
+    event :set_finished do
+      transitions to: :finished
+    end
+
+    event :set_closed do
+      transitions to: :closed
+    end
+
+    event :set_canceled do
+      transitions to: :cancelled
+    end
+  end
+
+
+  # Список фаз тестирования:
+  # 1. positioning_otkm => Позиционирование в ОТКМ (организации транспортного комплекса Москвы)
+  # 2. detailed_parameters => Детализация параметров пилотного тестирования
+  # 3. preparation_for_testing => Подготовка к пилотному тестированию
+  # 4. pilot_testing => Проведение пилотного тестирования
+  # 5. report_generation => Формирование отчёта пилотном тестировании
+  # 6. closing_pilot_project => Закрытие пилотного проекта
+  # 7. closed => Закрыт
+
+  aasm column: :testing_phase do
+    state :positioning_otkm, initial: true
+    state :detailed_parameters, :preparation_for_testing, :pilot_testing, :report_generation,
+          :closing_pilot_project, :closed
+
+    event :set_detailed_parameters do
+      transitions to: :detailed_parameters
+    end
+
+    event :set_preparation_for_testing do
+      transitions to: :preparation_for_testing
+    end
+
+    event :set_pilot_testing do
+      transitions to: :pilot_testing
+    end
+
+    event :set_report_generation do
+      transitions to: :report_generation
+    end
+
+    event :set_closing_pilot_project do
+      transitions to: :closing_pilot_project
+    end
+
+    event :set_closed do
+      transitions to: :closed
+    end
+  end
 end
