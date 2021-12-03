52952df87e4e34b1bb98690f6b7de29b644f3415
Александр Герасимов <andarelph@gmail.com>
2021-12-04 02:07:13 +0400

    Update data

--- frontend/src/pages/analytics/index.js
+++ frontend/src/pages/analytics/index.js
@@ -10,37 +10,37 @@ const grapthics = [
     type: 'by_state',
     title: 'Статусы стартапов',
     data: [{x: 'Идея', y: 10}, {x: 'Продукт', y: 2}, {x: 'Прототип', y: 15}],
-    span: 12,
+    span: 8,
   },
   {
     type: 'by_segment',
     title: 'Сегмент',
     data: [{x:'B2B', y: 11}, {x:'B2C', y: 12}, {x:'B2G', y: 1}, {x:'B2O', y: 6}],
-    span: 12,
+    span: 8,
   },
   {
     type: 'by_organization',
     title: 'Организации МТ',
     data: [{x: 'Московский метрополитен', y: 23}, {x: 'Мосгорстранс', y: 12}, {x:'ЦОДД', y: 23}, {x:'Организатор перевозок', y: 23}, {x: "Мостранспроект", y: 10}],
-    span: 12,
+    span: 8,
   },
   {
     type: 'by_people_count',
     title: 'Количество человек в организации',
     data: [{ x: '0-10', y: 70 }, { x: '10-20', y: 40 }, { x: "20-100", y: 10 }, { x: '>100', y: 5}],
-    span: 12,
+    span: 8,
   },
   {
     type: 'by_sphere_tags',
     title: 'Сферы применения',
     data: [{ x: 'Транспортные системы', y: 60 }, { x: 'Доступная среда', y: 40 }, { x: "Безопасность", y: 30 }, { x: 'Возобновляемая энергетика', y: 30}, { x: "Экология", y: 20}],
-    span: 24,
+    span: 8,
   },
   {
     type: 'by_tech_tags',
     title: 'Технологии',
     data: [{ x: 'Mobile', y: 40 }, { x: 'Web', y: 50 }, { x: "Big Data", y: 30 }, { x: 'ML', y: 50}, { x: "iOS", y: 10}],
-    span: 24,
+    span: 8,
   },
 ];
 
@@ -81,7 +81,7 @@ export function Analytics() {
                   width={400} height={400}
                   data={item.data}
                   innerRadius={68} labelRadius={100}
-                  style={{ labels: { fontSize: 20, fill: "white" } }}
+                  style={{ labels: { fontSize: 20, fill: "#1890ff" } }}
                 />
                 <VictoryLabel
                   textAnchor="middle"
